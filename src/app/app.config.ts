fd343bd16b73287a5b721f9ace7ac63595cd4ace
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-01-06 16:01:09 -0500

    feat: :sparkles: add material ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimationsAsync(),
     importProvidersFrom(
       FormsModule,
       ReactiveFormsModule
-    )
+    ),
   ]
 };
